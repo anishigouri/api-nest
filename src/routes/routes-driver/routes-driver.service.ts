9156ac94fb9da1ed17f39ff441dae010c4077119
Allan Nishigouri <allan.nishigouri@codex.com.br>
2023-08-25 19:04:55 +0000

    atualização de status do motorista

---
+++ src/routes/routes-driver/routes-driver.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+
+@Injectable()
+export class RoutesDriverService {
+  constructor(
+    private prismaService: PrismaService
+  ){}
+
+  async createOrUpdate(dto: {
+    route_id: string;
+    lat: number;
+    lng: number;
+  }) {
+    // const countRouteDriver = await this.prismaService.routeDriver.count({
+    //   where: {
+    //     route_id: dto.route_id
+    //   }
+    // });
+
+    return await this.prismaService.routeDriver.upsert({
+      where: {
+        route_id: dto.route_id
+      },
+      create: {
+        route_id: dto.route_id,
+        points: {
+          set: {
+            location: {
+              lat: dto.lat,
+              lng: dto.lng
+            }
+          }
+        }
+      },
+      update: {
+        points: {
+          push: {
+            location: {
+              lat: dto.lat,
+              lng: dto.lng
+            }
+          }
+        }
+      }
+    })
+  }
+}
